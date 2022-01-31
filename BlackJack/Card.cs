3509f1cf864ece6fbaf9a43a624115456cb09265
Charlie Sesser Nylund <charlie.sessernylund@elev.ga.ntig.se>
2022-01-31 09:50:34 +0100

    many tings

--- BlackJack/Card.cs
+++ BlackJack/Card.cs
@@ -5,42 +5,73 @@ namespace BlackJack
 {
     public class Card
     {
+        public static List<string> allCards = ReturnAllCards();
+
+        public static List<string> activeCards = new List<string>();
 
         private Random cardShuffler = new Random();
-
-        private List<string> CardsNumber = new List<string> { "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2" };
-        private List<string> CardID = new List<string> { };
 
         private string cardName;
 
         private int cardValue;
 
-
-
-        public List<string> ReturnAllCards()
+        public Card()
         {
-            string[] type = new string[] { "h", "c", "e", "g" };
+            allCards = ReturnAllCards();
+        }
+
+        private static List<string> ReturnAllCards()
+        {
+            string[] type = new string[] { "H", "C", "S", "D" };
 
             List<string> allcards = new List<string>();
 
             for (int i = 0; i < type.Length; i++)
             {
-                for (var y = 0; y < 13; y++)
+                for (var y = 1; y < 14; y++)
                 {
-                    allcards.Add(type[i] + "" + y);
+                    allcards.Add(type[i] + y);
                 }
             }
             return allcards;
         }
-        public string WriteCardName(string card)
+
+        public static string WriteCardName(string card)
         {
-            string name;
+            string name = card.Substring(1);
 
-            name = card.Substring(1);
-
-            if (card.StartsWith("h"))
+            if (name.Substring(1) == "1")
             {
-                name = name + "of Hearts";
+                name = "Ace";
+            }
+            else if (name.Substring(1) == "11")
+            {
+                name = "Jack";
+            }
+            else if (name.Substring(1) == "12")
+            {
+                name = "Queen";
+            }
+            else if (name.Substring(1) == "13")
+            {
+                name = "King";
+            }
+
+            if (card.StartsWith("H"))
+            {
+                name += " of Hearts";
+            }
+            if (card.StartsWith("C"))
+            {
+                name += " of Clubs";
+            }
+            if (card.StartsWith("S"))
+            {
+                name += " of Spades";
+            }
+            if (card.StartsWith("D"))
+            {
+                name += " of Diamonds";
             }
             return name;
         }
--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -6,7 +6,30 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Black jack!");
+
+            Random generator = new Random();
+
+            Console.WriteLine($@".______    __          ___       ______  __  ___           __       ___       ______  __  ___ 
+|   _  \  |  |        /   \     /      ||  |/  /          |  |     /   \     /      ||  |/  / 
+|  |_)  | |  |       /  ^  \   |  ,----'|  '  /           |  |    /  ^  \   |  ,----'|  '  /  
+|   _  <  |  |      /  /_\  \  |  |     |    <      .--.  |  |   /  /_\  \  |  |     |    <   
+|  |_)  | |  `----./  _____  \ |  `----.|  .  \     |  `--'  |  /  _____  \ |  `----.|  .  \  
+|______/  |_______/__/     \__\ \______||__|\__\     \______/  /__/     \__\ \______||__|\__\ ");
+
+            Console.WriteLine("These are the rules of Blackjack:");
+            Console.WriteLine("Each participant attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.");
+            Console.WriteLine("The Dealer will get two cards, one will be hidden. Then the player will get two cards.");
+            Console.WriteLine("The player can decide if they want to hit or stand, if they hit they get another card, if they stand the dealer shows their hidden card");
+
+            Card c1 = new Card();
+
+            // Console.WriteLine(c1.WriteCardName("H"));
+
+            // foreach (string card in Card.allCards)
+            // {
+            Console.WriteLine(Card.WriteCardName(Card.allCards[generator.Next(53)]));
+
+            Console.ReadLine();
         }
     }
 }
